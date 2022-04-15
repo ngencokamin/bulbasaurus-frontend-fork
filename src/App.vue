6db887b81c6b73e23f53f0229ca373c1bc97a827
CierraSimms <cierrasimms98@gmail.com>
2022-04-14 23:13:21 -0400

    added PokemonShow

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,106 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-light" style="background-color: lightpink">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/products">Pokemon</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/products/new">New Product</a>
+          </li>
+          <li v-if="!isLoggedIn" key="" class="nav-item">
+            <a class="nav-link" href="/signup">Sign Up</a>
+          </li>
+          <li v-if="!isLoggedIn" class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <a class="nav-link" href="/logout">Logout</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/orders">Order</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
+
   <router-view />
 </template>
 
+<script>
+export default {
+  data: function () {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  watch: {
+    $route: function () {
+      this.isLoggedIn = !!localStorage.jwt;
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+h1 {
+  font-size: 24px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 700;
+  line-height: 26.4px;
+  text-decoration: underline;
 }
-
-nav {
-  padding: 30px;
+h3 {
+  font-size: 14px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 700;
+  line-height: 15.4px;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+p {
+  font-size: 20px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 20px;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+blockquote {
+  font-size: 21px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 30px;
+}
+pre {
+  font-size: 13px;
+  font-style: normal;
+  font-variant: normal;
+  font-weight: 400;
+  line-height: 18.5714px;
+}
+body {
+  background-image: url("https://previews.123rf.com/images/hanganuanamaria/hanganuanamaria1906/hanganuanamaria190600022/126279062-sfondo-astratto-con-colori-chiari-e-pastello-carta-da-parati-semplice-sfondo-per-la-presentazione-de.jpg");
+}
+body {
+  font-family: "Zilla Slab", serif;
+  font-size: 20px;
+}
+input {
+  caret-color: deeppink;
 }
 </style>
---
+++ src/views/PokemonShow.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <h1>Pokemon Info</h1>
+    <h2>{{ pokemon.name }}</h2>
+    <p>{{ pokemon.id }}</p>
+    <p>{{ pokemon.pokemon_type }}</p>
+    <p>{{ pokemon.species }}</p>
+    <p>{{ pokemon.height }}</p>
+    <p>{{ pokemon.hp }}</p>
+    <p>{{ pokemon.attack }}</p>
+    <p>{{ pokemon.defense }}</p>
+    <p>{{ pokemon.spec_attack }}</p>
+    <p>{{ pokemon.spec_defense }}</p>
+    <p>{{ pokemon.speed }}</p>
+    <p>{{ pokemon.total }}</p>
+    <img
+      v-if="pokemon && pokemon.images && pokemon.images[0]"
+      v-bind:src="pokemon.images[0].url"
+      v-bind:alt="pokemon.name"
+      style="max-width: 250px"
+    />
+    <div></div>
+    <router-link to="/pokemon">Back to all Pokemon!</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      pokemon: [],
+    };
+  },
+  created: function () {
+    this.showPokemon();
+  },
+  methods: {
+    showPokemon() {
+      axios.get(`/pokemon/${this.$route.params.id}`).then((response) => {
+        this.pokemon = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: rgb(61, 1, 101);
+  text-shadow: 2px 2px #00eeff;
+}
+h2 {
+  color: rgb(0, 0, 0);
+}
+p {
+  color: rgb(0, 0, 0);
+}
+</style>
