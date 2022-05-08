f6130d9836c4bff6080dd062b4badc9bfeb12e83
CierraSimms <cierrasimms98@gmail.com>
2022-05-08 17:00:37 -0400

    added base stats and evolution chain

--- src/views/PokemonShow.vue
+++ src/views/PokemonShow.vue
@@ -10,11 +10,39 @@
     </div>
     <h2>{{ currentPokemon.name }}</h2>
     <p>Types: {{ attribute[0] }}, {{ attribute[1] }}</p>
-    <p>Category: {{ currentPokemon.pokemon_species }}</p>
+    <p>Under a full moon, this POKéMON likes to mimic the shadows of people and laugh at their fright.</p>
+    <p>Category: Shadow Pokemon</p>
     <p>Height: {{ currentPokemon.height }}</p>
     <p>Weight: {{ currentPokemon.weight }}</p>
-    <p v-if="species?.shape">Shape: {{ species.shape }}</p>
     <p v-if="ability?.name">Abilities: {{ ability.name }}</p>
+  </div>
+  <div>
+    <h3>Evolution Chain</h3>
+    <img
+      src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pm/5c619391fb78cbe0d3646a9b76da07372a18580e.png"
+      id="left"
+      style="max-width: 250px"
+    />
+    <img
+      src="https://archives.bulbagarden.net/media/upload/thumb/6/62/093Haunter.png/250px-093Haunter.png"
+      id="center"
+      style="max-width: 250px"
+    />
+
+    <img
+      src="https://archives.bulbagarden.net/media/upload/thumb/c/c6/094Gengar.png/250px-094Gengar.png"
+      id="right"
+      style="max-width: 250px"
+    />
+  </div>
+  <div>
+    <h3>Base Stats</h3>
+    <p>HP: {{ stats[0] }}</p>
+    <p>Attack: {{ stats[1] }}</p>
+    <p>Defense: {{ stats[2] }}</p>
+    <p>Special Attack: {{ stats[3] }}</p>
+    <p>Special Defense: {{ stats[4] }}</p>
+    <p>Speed: {{ stats[5] }}</p>
   </div>
   <router-link to="/pokemon">Back to all Pokemon!</router-link>
 </template>
@@ -29,13 +57,13 @@ export default {
       currentPokemon: {},
       ability: {},
       attribute: [],
-      species: {},
+      stats: [],
+      base_stat: [],
     };
   },
   mounted: function () {
     this.showPokemon();
     this.abilityShow();
-    this.speciesShow();
   },
   methods: {
     showPokemon() {
@@ -45,6 +73,12 @@ export default {
         attribute.forEach((type) => {
           this.attribute.push(type.type.name);
         });
+        let stats = response.data.stats;
+        stats.forEach((base_stat) => {
+          this.stats.push(base_stat.base_stat);
+        });
+
+        console.log("UGHHHH", this.stats);
         console.log("AHHH!!!", this.attribute);
         console.log(this.currentPokemon);
 
@@ -55,12 +89,6 @@ export default {
       axios.get("https://pokeapi.co/api/v2/ability/" + this.$route.params.id).then((response) => {
         this.ability = response.data;
         console.log("THIS IS THE ABILITY", this.ability);
-      });
-    },
-    speciesShow() {
-      axios.get("https://pokeapi.co/api/v2/pokemon-species/" + this.$route.params.id).then((response) => {
-        this.species = response.data;
-        console.log("THIS IS THE SPECIES", this.species);
       });
     },
   },
@@ -84,4 +112,12 @@ p {
   margin-right: auto;
   width: 50%;
 }
+#center {
+}
+#left {
+  float: left;
+}
+/* #right {
+  float: right;
+} */
 </style>
