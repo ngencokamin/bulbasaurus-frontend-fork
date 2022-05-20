b22e93f027d0057d8f75c05ecb20960fe0f86080
CierraSimms <cierrasimms98@gmail.com>
2022-05-19 22:10:45 -0400

    added heroku

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 
-// axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
-// var jwt = localStorage.getItem("jwt");
-// if (jwt) {
-//   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
-// }
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://hidden-badlands-66386.com";
-  var jwt = localStorage.getItem("jwt");
+var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://hidden-badlands-66386.com";
+//   var jwt = localStorage.getItem("jwt");
+// if (jwt) {
+//   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
 
 createApp(App).use(router).mount("#app");
